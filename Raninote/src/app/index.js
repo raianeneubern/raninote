eb36d35a6c4dfd7e79bbc2d4d095ccabe28d5be6
NicolasRDuarte <drnicolasribeiroduarte@gmail.com>
2024-05-02 11:38:24 -0300

    Finalização da tela de bem-vindo

--- Raninote/src/app/index.js
+++ Raninote/src/app/index.js
@@ -8,11 +8,18 @@ export default function Welcome() {
       <View style={styles.containerImage}>
         <Image source={require('../assets/avatar.jpeg')} style={styles.logo} />
       </View>
-      <Text>Welcome</Text>
-      <Link href={"/home"}>
-        <Text>Acessar</Text>
-      </Link>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Raninote
+        </Text>
+        <Text style={styles.text}>
+          Monte a sua lista de tarefas e não esqueça mais o que precisa ser feito!!!
+        </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
+      <StatusBar style="light"  backgroundColor='#000'/>
     </View>
   );
 }
@@ -33,4 +40,37 @@ const styles = StyleSheet.create({
     borderRadius: 115,
     resizeMode: 'cover',
   },
+  content: {
+    flex: 1,
+    backgroundColor: "#dadada",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  text: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    bottom: '15%',
+    alignSelf: 'center',
+    borderRadius: 50,
+    paddingVertical: 15,
+    width: '60%',
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
