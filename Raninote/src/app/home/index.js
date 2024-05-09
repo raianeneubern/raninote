d2e108a72172197c7725cf489e3674138b620f65
raianeneubern <raianeneubern@gmail.com>
2024-05-09 11:03:32 -0300

    Estilizando a lista de produtos

--- Raninote/src/app/home/index.js
+++ Raninote/src/app/home/index.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity
 } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
+import { styles } from './style';
 
 export default function index() {
   return (
@@ -17,7 +18,23 @@ export default function index() {
         resizeMode='repeat'
         style={{ flex: 1, justifyContent: 'flex-start' }}
       >
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de Produtos</Text>
+          <View>
+            <Ionicons name="trash" size={32} color="#fff" />
+          </View>
+        </View>
 
+        {/* Lista de Produtos */}
+
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name="add" size={32} color="#fff" />
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
     </SafeAreaView>
   )
---
+++ Raninote/src/app/home/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    header: {
+        padding: 25, 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#000000c0',
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+    title: {
+        fontSize: 26,
+        color: '#fff',
+        fontWeight: 'bold',
+        color: '#ffffff',
+    }
+});
