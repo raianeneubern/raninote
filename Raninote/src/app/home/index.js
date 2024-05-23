7c3baab6594c57c44609329fc485a07a970b53fe
raianeneubern <raianeneubern@gmail.com>
2024-05-23 11:40:16 -0300

    Finalizada a interface

--- Raninote/src/app/home/index.js
+++ Raninote/src/app/home/index.js
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
   View, 
   Text, 
   SafeAreaView, 
   ImageBackground,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList,
+  Alert
 } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
 import { styles } from './style';
 import ItemList from '../../componentes/ItemList';
+import { StatusBar } from 'expo-status-bar';
+import AsynnStorage from '@react-native-async-storage/async-storage';
 
 export default function index() {
+  const [textInput, setTextInput] = useState('')
+  const [items, setItems] = useState([]);
+
+  const saveItemToDevice = async () => {
+
+  }
+
+  const getItemsFromDevice = async () => {
+
+  }
+
+  const addItem = () => {
+
+  }
+
+  const markItemBought = itemId => {
+
+  }
+
+  const unmarkItemBought = itemId => {
+
+  }
+
+  const removeItem = itemId => {
+
+  }
+
+  const removeAll = () => {
+    Alert.alert("Limpar Lista?", "Confirma a exclusão de todos os produtos de sua lista?",
+    [{
+      text: 'Sim',
+      onPress: () => { setItems([]) }
+    }, {
+      text: 'Cancelar',
+      style: 'cancel',
+    }]
+    )
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ImageBackground 
@@ -22,13 +65,22 @@ export default function index() {
         <View style={styles.header}>
           <Text style={styles.title}>Lista de Produtos</Text>
           <View>
-            <Ionicons name="trash" size={32} color="#fff" />
+            <Ionicons name="trash" size={32} color="#fff" onPress={removeAll} />
           </View>
         </View>
 
         {/* Lista de Produtos */}
-        <ItemList item={"Café"}></ItemList>
-        <ItemList item={"Chocolate"}></ItemList>
+        <FlatList 
+          contentContainerStyle={{ padding: 20, paddingBottom: 100, color: '#fff'}}
+          data={items}
+          renderItem={({item}) =>
+            <ListItem item={item} 
+            markItem={markItemBought}
+            unmarkItem={unmarkItemBought}
+            removeItem={removeItem}
+          />
+        }
+        />
 
         <View style={styles.footer}>
           <View style={styles.inputContainer}>
@@ -37,9 +89,11 @@ export default function index() {
               fontSize={18}
               placeholderTextColor="#aeaeae"
               placeholder="Digite o nome do produto..."
+              value={textInput}
+              onChange={(text) => setTextInput(text)}
             />
           </View>
-          <TouchableOpacity style={styles.iconContainer}>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
             <Ionicons name="add" size={32} color="#fff" />
           </TouchableOpacity>
         </View>
--- Raninote/src/componentes/ItemList/index.js
+++ Raninote/src/componentes/ItemList/index.js
@@ -7,9 +7,32 @@ export default function ItemList({ item, markItem, unmarkItem, removeItem }) {
   return (
     <View style={styles.itemList}>
         <View style={{ flex : 1 }}>
-            <Text style={styles.itemToBuy}>{item}</Text>
+            <Text style={item?.bought ? styles.itemBought : styles.itemToBuy}>
+              {item?.name}
+            </Text>
         </View>
-      
+        {!item?.bought ? (
+          <TouchableOpacity
+            style={styles.actionIcon}
+            onPress={() => markItem(item)}
+          >
+            <Ionicons name='bag-check-outline' size={24} color='#fff'/>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            style={styles.actionIcon}
+            onPress={() => unmarkItem(item)}
+          >
+            <Ionicons name='bag-remove-outline' size={24} color='#fff'/>
+          </TouchableOpacity>
+        ) }
+
+        <TouchableOpacity
+            style={[styles.actionIcon, { backgroundColor: 'darkred'}]}
+            onPress={() => removeItem(item)}
+        >
+            <Ionicons name='trash-bin-outline' size={24} color='#fff'/>
+        </TouchableOpacity>
     </View>
   )
 }
